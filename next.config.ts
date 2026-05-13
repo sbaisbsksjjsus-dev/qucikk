666dbb5eca776a1db0ec7d53ec40df0e40bceed4
sbaisbsksjjsus-dev <sbaisbsksjjsus@gmail.com>
2026-05-13 04:39:57 +0000

    Update Next.js export config for static deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // 开启静态导出
+  images: {
+    unoptimized: true, // 静态导出不支持 Next.js 图片优化，需开启此项
+  },
 };
 
 export default nextConfig;
