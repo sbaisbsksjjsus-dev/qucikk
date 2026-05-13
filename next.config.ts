48d5ffc0ab467d3ce1ea979bb35e93ce7345a548
sbaisbsksjjsus-dev <sbaisbsksjjsus@gmail.com>
2026-05-13 06:44:01 +0000

    Remove ignored build artifacts and clean workspace

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // 开启静态导出
+  output: 'export',
   images: {
-    unoptimized: true, // 静态导出不支持 Next.js 图片优化，需开启此项
+    unoptimized: true,
   },
 };
 
